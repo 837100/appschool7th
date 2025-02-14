72ff778bfb4dfd1f98596c170d6d2062ac7a8843
837100 <shtjdrud0830@naver.com>
2025-02-14 10:31:45 +0900

    SharedPreferenceDemo를 상태ful 위젯으로 변경하고 사용자 이름 로드 기능 추가

--- 05-DartandFlutter/list_navigation/lib/shared_preference_demo.dart
+++ 05-DartandFlutter/list_navigation/lib/shared_preference_demo.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SharedPreferenceDemo extends StatelessWidget {
-  SharedPreferenceDemo({super.key});
+class SharedPreferenceDemo extends StatefulWidget {
+  const SharedPreferenceDemo({super.key});
 
+  @override
+  State<SharedPreferenceDemo> createState() => _SharedPreferenceDemoState();
+}
+
+class _SharedPreferenceDemoState extends State<SharedPreferenceDemo> {
   final TextEditingController _controller = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _loadUsername();
+  }
+
+  void _loadUsername() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      _controller.text = prefs.getString('username') ?? '';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Shared Preference Demo')),
-        body: Center(
-            child: TextField(
-          decoration: const InputDecoration(
-            labelText: 'Enter your username',
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Enter your username',
+                ),
+                controller: _controller,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  final SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  prefs.setString('username', _controller.text);
+                },
+                child: const Text('Save'),
+              ),
+            ],
           ),
-          controller: _controller,
-        )));
+        ));
   }
 }
