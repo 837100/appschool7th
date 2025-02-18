36e7e069549fe299df7a389bb6080f572f304e36
837100 <shtjdrud0830@naver.com>
2025-02-18 14:05:35 +0900

    데이터 로딩 방식을 개선하고 FutureBuilder를 사용하여 비동기 데이터 처리 방식 수정

--- 05-DartandFlutter/my_data_demo/lib/3_with_file_and_json.dart
+++ 05-DartandFlutter/my_data_demo/lib/3_with_file_and_json.dart
@@ -7,6 +7,7 @@ class DataItem {
   DataItem({required this.title});
 
   factory DataItem.fromJson(Map<String, dynamic> json) {
+    // 키밸류의 타이틀만 가져와서 객체를 생성
     return DataItem(title: json['title']);
   }
 }
@@ -17,8 +18,10 @@ class DataSeries {
   DataSeries({required this.dataModel});
 
   factory DataSeries.fromJson(Map<String, dynamic> json) {
+    // assets의 example2 의 data as List로 List로 타입캐스팅
     var list = json['data'] as List;
     List<DataItem> dataList =
+        // map 하나하나 돌면서 뒤의 함수 실행. map은 Iterable데이터로 묶어짐. 반복문으로 수행될수 있도록 컬렉션으로 되어 있는데 tolist로 list로 변환.
         list.map((dataModel) => DataItem.fromJson(dataModel)).toList();
     return DataSeries(dataModel: dataList);
   }
@@ -32,15 +35,22 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late Future<DataSeries> dataSeries;
+  // late Future<DataSeries> dataSeries;
+  DataSeries? dataSeries;
 
   @override
   void initState() {
     super.initState();
-    dataSeries = fetchData();
+    // dataSeries = fetchData();
+    fetchData().then((value) {
+      setState(() {
+        dataSeries = value;
+      });
+    });
   }
 
   Future<DataSeries> fetchData() async {
+    // 파일을 가져오는 것이 컴퓨터 입장에서 오래걸리는 작업이므로 await
     String jsonString = await DefaultAssetBundle.of(
       context,
     ).loadString('assets/example2.json');
@@ -57,18 +67,25 @@ class _MyAppState extends State<MyApp> {
       ),
       home: Scaffold(
         appBar: AppBar(title: const Text('파일 데이터를 JSON으로 가져오기')),
-        body: FutureBuilder<DataSeries>(
-          future: dataSeries,
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            final data = snapshot.data!;
-            return ListView.builder(
-              itemCount: data.dataModel.length,
-              itemBuilder: (context, index) {
-                return ListTile(title: Text(data.dataModel[index].title));
-              },
+        // body: FutureBuilder<DataSeries>(
+        //   future: dataSeries,
+        //   builder: (context, snapshot) {
+        //     if (!snapshot.hasData) {
+        //       return const Center(child: CircularProgressIndicator());
+        //     }
+        //     final data = snapshot.data!;
+        //     return ListView.builder(
+        //       itemCount: data.dataModel.length,
+        //       itemBuilder: (context, index) {
+        //         return ListTile(title: Text(data.dataModel[index].title));
+        //       },
+        body: ListView.builder(
+          itemCount: dataSeries?.dataModel.length ?? 0,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(dataSeries?.dataModel[index].title ?? ''),
+
+              // }),
             );
           },
         ),
--- 05-DartandFlutter/my_data_demo/lib/main.dart
+++ 05-DartandFlutter/my_data_demo/lib/main.dart
@@ -1,62 +1,75 @@
-import 'dart:convert';
+// import 'dart:convert';
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   late Future<List<String>> futureData;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     futureData = _loadMonths();
+//   }
+
+//   Future<List<String>> _loadMonths() async {
+//     debugPrint('debug.month');
+//     String jsonString = await DefaultAssetBundle.of(
+//       context,
+//     ).loadString('assets/example.json');
+//     debugPrint('jsonString: $jsonString');
+//     return jsonDecode(jsonString).values.cast<String>().toList();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//       ),
+//       home: Scaffold(
+//         appBar: AppBar(title: const Text('My Data App')),
+//         body: FutureBuilder<List<String>>(
+//           future: futureData,
+//           builder: (context, snapshot) {
+//             if (!snapshot.hasData) {
+//               // 미래 데이터에 대해 값이 없는 경우 (1: 값이 안 넘어온 경우, 2: 로딩 중, 3: 에러)
+//               return const Center(child: CircularProgressIndicator());
+//             }
+//             final data = snapshot.data!;
+//             return ListView.builder(
+//               itemCount: data.length,
+//               itemBuilder: (context, index) {
+//                 return ListTile(title: Text(data[index]));
+//               },
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 import 'package:flutter/material.dart';
+// 파일로만 데이터를 가져오는 방법
+// import '1_basic.dart';
+
+// 파일과 함께 데이터를 가져오는 방법
+// import '2_with_file.dart';
+// 파일과 JSON을 사용하여 데이터를 가져오는 방법
+import '3_with_file_and_json.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late Future<List<String>> futureData;
-
-  @override
-  void initState() {
-    super.initState();
-    futureData = _loadMonths();
-  }
-
-  Future<List<String>> _loadMonths() async {
-    debugPrint('debug.month');
-    String jsonString = await DefaultAssetBundle.of(
-      context,
-    ).loadString('assets/example.json');
-    debugPrint('jsonString: $jsonString');
-    return jsonDecode(jsonString).values.cast<String>().toList();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('My Data App')),
-        body: FutureBuilder<List<String>>(
-          future: futureData,
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              // 미래 데이터에 대해 값이 없는 경우 (1: 값이 안 넘어온 경우, 2: 로딩 중, 3: 에러)
-              return const Center(child: CircularProgressIndicator());
-            }
-            final data = snapshot.data!;
-            return ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                return ListTile(title: Text(data[index]));
-              },
-            );
-          },
-        ),
-      ),
-    );
-  }
+  runApp(MyApp());
 }
