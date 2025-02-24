95d4b96020b3d3917ed22e989fe70e75519f581e
837100 <shtjdrud0830@naver.com>
2025-02-24 15:12:43 +0900

    파일 정리, 챕터 3 위젯 프로젝트 생성

--- flutter_project_book/unit_converter/lib/main.dart
+++ flutter_project_book/unit_converter/lib/main.dart
@@ -1,54 +1,54 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
+// class MyApp extends StatefulWidget {
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
 
-class _MyAppState extends State<MyApp> {
-  var fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
-  double _numberFrom = 0;
+// class _MyAppState extends State<MyApp> {
+//   var fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+//   double _numberFrom = 0;
 
-  @override
-  void initState() {
-    _numberFrom = 0;
-    super.initState();
-  }
+//   @override
+//   void initState() {
+//     _numberFrom = 0;
+//     super.initState();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Measures Converter',
-      home: Scaffold(
-        appBar: AppBar(title: Text('Measures Converter')),
-        DropdownButton<String>(
-          items: fruits.map((String value) {
-            return DropDownMenuItem<String>(
-              value: value,
-              child: Text(value),);
-            )
-          })
-        )
-        body: Center(
-          child: Column(
-            children: [
-              TextField(
-                onChanged: (text) {
-                  var rv = double.tryParse(text);
-                  if (rv != null) {
-                    setState(() {
-                      _numberFrom = rv;
-                    });
-                  }
-                },
-              ),
-              Text((_numberFrom == null) ? '' : _numberFrom.toString()),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Measures Converter',
+//       home: Scaffold(
+//         appBar: AppBar(title: Text('Measures Converter')),
+//         DropdownButton<String>(
+//           items: fruits.map((String value) {
+//             return DropDownMenuItem<String>(
+//               value: value,
+//               child: Text(value),);
+//   })
+//           ),
+//   );
+//         body: Center(
+//           child: Column(
+//             children: [
+//               TextField(
+//                 onChanged: (text) {
+//                   var rv = double.tryParse(text);
+//                   if (rv != null) {
+//                     setState(() {
+//                       _numberFrom = rv;
+//                     });
+//                   }
+//                 },
+//               ),
+//               Text((_numberFrom == null) ? '' : _numberFrom.toString()),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
