7992d1f646e33c5b13ccf3402fe548b738e8fdc2
837100 <shtjdrud0830@naver.com>
2025-02-25 11:13:08 +0900

    GridScreen에서 아이템 수를 20개에서 100개로 증가시키고, GridView.builder를 사용하여 그리드 레이아웃을 구현함

--- 06-Flutter/04-Layout/layout_demo/lib/grid_screen.dart
+++ 06-Flutter/04-Layout/layout_demo/lib/grid_screen.dart
@@ -3,13 +3,27 @@ import 'package:flutter/material.dart';
 class GridScreen extends StatelessWidget {
   GridScreen({super.key});
 
-  final List<String> items = List.generate(20, (index) => 'Item ${index + 1}');
+  final List<String> items = List.generate(100, (index) => 'Item ${index + 1}');
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('GridView 예제')),
-        body: const Placeholder(),
+        body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            var item = items[index];
+            return Container(
+              color: Colors.blue[100 * ((index % 9) + 1)],
+              child: Center(child: Text(item)),
+            );
+          },
+        ),
       ),
     );
   }
