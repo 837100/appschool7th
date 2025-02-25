1055d7e9882a0b19f9eed44fb669cc33821c6744
837100 <shtjdrud0830@naver.com>
2025-02-25 10:53:07 +0900

    플러터 리스트 예제 코드

---
+++ 06-Flutter/04-Layout/layout_demo/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:layout_demo/list_screen.dart';
+
+void main() {
+  runApp(ListScreen());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('반응형 UI 예제')),
+        body: Column(
+          children: [
+            LayoutBuilder(
+              builder: (context, BoxConstraints constraints) {
+                double fontSize = constraints.maxWidth > 600 ? 32 : 24;
+                debugPrint('화면 너비: ${constraints.maxWidth.toStringAsFixed(2)}');
+                debugPrint(
+                  '화면 높이: ${constraints.maxHeight.toStringAsFixed(2)}',
+                );
+                debugPrint('텍스트 크기: $fontSize');
+
+                return Center(
+                  child: Text('반응형 텍스트', style: TextStyle(fontSize: fontSize)),
+                );
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: EdgeInsets.all(0),
+                    color: Colors.red,
+                    child: Text('위젯 1'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: EdgeInsets.all(0),
+                    color: Colors.green,
+                    child: Text('위젯 2'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
