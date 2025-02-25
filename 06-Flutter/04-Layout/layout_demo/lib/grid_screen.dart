8faa58a7dc38929408cbcbd1c87a9274dcaf3b37
837100 <shtjdrud0830@naver.com>
2025-02-25 11:39:57 +0900

    RawScrollbar로 스크롤바를 변경하고, 스크롤 방향을 수직으로 설정하여 GridView의 레이아웃을 개선함

--- 06-Flutter/04-Layout/layout_demo/lib/grid_screen.dart
+++ 06-Flutter/04-Layout/layout_demo/lib/grid_screen.dart
@@ -12,24 +12,35 @@ class GridScreen extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('GridView 예제')),
-        body: Scrollbar(
+        body: RawScrollbar(
+          // body: Scrollbar(
           controller: _scrollController,
-          child: GridView.builder(
-            scrollDirection: Axis.horizontal,
-            controller: _scrollController,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
+          thumbVisibility: true,
+          thumbColor: Colors.grey,
+          // trackVisibility: true,
+          minThumbLength: 500,
+          thickness: 8.0, // 스크롤바의 굵기 설정
+          radius: Radius.circular(30.0), // 스크롤바의 모서리 반경 설정
+
+          child: Padding(
+            padding: const EdgeInsets.only(right: 1.0),
+            child: GridView.builder(
+              scrollDirection: Axis.vertical,
+              controller: _scrollController,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                var item = items[index];
+                return Container(
+                  color: Colors.blue[100 * ((index % 9) + 1)],
+                  child: Center(child: Text(item)),
+                );
+              },
             ),
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              var item = items[index];
-              return Container(
-                color: Colors.blue[100 * ((index % 9) + 1)],
-                child: Center(child: Text(item)),
-              );
-            },
           ),
         ),
       ),
