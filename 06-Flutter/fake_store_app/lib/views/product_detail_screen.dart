a8d558f526f71dc4db88d70f1e35bccf8dc97612
837100 <shtjdrud0830@naver.com>
2025-02-26 14:41:25 +0900

    상품 상세 화면에 수량 선택기 추가 및 '장바구니에 담기' 버튼 항상 보이도록 변경

--- 06-Flutter/fake_store_app/lib/views/product_detail_screen.dart
+++ 06-Flutter/fake_store_app/lib/views/product_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:fake_store_app/models/product.dart';
 import 'package:fake_store_app/services/api_service.dart';
+import 'package:fake_store_app/widgets/quantity_sclector.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetailScreen extends StatefulWidget {
@@ -23,17 +24,42 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('상품 상세')),
-      body: FutureBuilder(
-        future: product,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasError) {
-            return const Center(child: Text('상품을 불러오는 중에 오류가 발생했습니다.'));
-          }
-          return _buildProductDetail(snapshot.data as Product);
-        },
+      body: SafeArea(
+        child: FutureBuilder(
+          future: product,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return const Center(child: Text('상품을 불러오는 중에 오류가 발생했습니다.'));
+            }
+            return Column(
+              children: [
+                Expanded(child: _buildProductDetail(snapshot.data as Product)),
+
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      '장바구니에 담기',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
@@ -76,21 +102,8 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
             ),
             const SizedBox(height: 24),
 
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  foregroundColor: Colors.white,
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  '장바구니에 담기',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
+            QuantitySclector(),
+            const SizedBox(height: 24),
           ],
         ),
       ),
---
+++ 06-Flutter/fake_store_app/lib/widgets/quantity_sclector.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuantitySclector extends StatefulWidget {
+  const QuantitySclector({super.key});
+
+  @override
+  State<QuantitySclector> createState() => _QuantitySclectorState();
+}
+
+class _QuantitySclectorState extends State<QuantitySclector> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: 30, child: const Placeholder());
+  }
+}
