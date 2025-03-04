9980583e4d62eb7bc6d5c7a8a42951fb085191dc
837100 <shtjdrud0830@naver.com>
2025-03-04 17:33:14 +0900

    안드로이드 Stripe 결제 창 로딩 안되는 것 해결

--- 06-Flutter/fake_store_app/lib/services/api_service.dart
+++ 06-Flutter/fake_store_app/lib/services/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 
 import '../models/product.dart';
@@ -6,7 +7,9 @@ import '../models/product.dart';
 class ApiService {
   final String _baseUrl = 'https://fakestoreapi.com';
   final String _simpleServerUrl = 'http://localhost:3001';
-  final String _androidServerUrl = 'http://10.0.0.2:3001';
+
+  // 안드로이드 애뮬레이터 실행을 위한 url
+  final String _androidServerUrl = 'http://10.0.2.2:3001';
 
   Future<List<Product>> getProducts() async {
     final response = await http.get(Uri.parse('$_baseUrl/products'));
@@ -31,7 +34,11 @@ class ApiService {
 
   Future<String> createPaymentIntent() async {
     final response = await http.post(
-      Uri.parse('$_simpleServerUrl/create-payment-intent'),
+      Uri.parse(
+        Platform.isAndroid
+            ? '$_androidServerUrl/create-payment-intent'
+            : '$_simpleServerUrl/create-payment-intent',
+      ),
     );
     if (response.statusCode == 200) {
       return jsonDecode(response.body)['clientSecret'];
