2f08960f8a40ad9265c2dca7629bd3457cc01157
837100 <shtjdrud0830@naver.com>
2025-03-04 23:57:05 +0900

    결제 완료 화면 왼쪽으로 쏠려있던 문제 해결

--- 06-Flutter/fake_store_app/lib/screens/order_confirmation_screen.dart
+++ 06-Flutter/fake_store_app/lib/screens/order_confirmation_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
 import 'package:provider/provider.dart';
 import '../models/order.dart';
 import '../providers/order_provider.dart';
@@ -17,33 +17,36 @@ class OrderConfirmationScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('결제 완료')),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Spacer(),
-            Icon(Icons.check_circle, color: Colors.green, size: 100),
-            Text('결제가 완료되었습니다.', style: TextStyle(fontSize: 20)),
-            Card(
-              margin: EdgeInsets.all(16),
-              child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  children: [
-                    Text('주문번호: ${order?.id}'),
-                    Text('결제 금액: \$${order?.totalAmount.toStringAsFixed(2)}'),
-                    Text('결제 일시: ${order?.date}'),
-                  ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Spacer(),
+              Icon(Icons.check_circle, color: Colors.green, size: 100),
+              Text('결제가 완료되었습니다.', style: TextStyle(fontSize: 20)),
+              Card(
+                margin: EdgeInsets.all(16),
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Text('주문번호: ${order?.id}'),
+                      Text('결제 금액: \$${order?.totalAmount.toStringAsFixed(2)}'),
+                      Text('결제 일시: ${order?.date}'),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Spacer(),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).popUntil((route) => route.isFirst);
-              },
-              child: const Text('홈으로'),
-            ),
-          ],
+              Spacer(),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).popUntil((route) => route.isFirst);
+                },
+                child: const Text('홈으로'),
+              ),
+            ],
+          ),
         ),
       ),
     );
