c50efb084ecddc9d59258d580e41ae5956a48aec
837100 <no8371@gmail.com>
2025-03-19 11:14:49 +0900

    챕터 11 세그웨이를 이용한 뷰 컨트롤러 전환 Main.storyboard에서 show segue를 선택, Custom Class에서 클래스를 전환할 뷰로 설정

--- 07-UIKit/NavigationDemo/NavigationDemo/ViewController.swift
+++ 07-UIKit/NavigationDemo/NavigationDemo/ViewController.swift
@@ -20,8 +20,9 @@ class ViewController: UIViewController {
         self.navigationController?.navigationBar.barTintColor = .systemBlue
         self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
         // 네비게이션바의 큰 타이틀 설정
-//        self.navigationController?.navigationBar.prefersLargeTitles = true
-//        self.navigationController?.navigationBar.backgroundColor = .systemBlue
+        //        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.backgroundColor = .systemBlue
+        setupButton()
         
         
         //        // iOS 15 이상에서는 UINavigationBarAppearance 사용
@@ -41,15 +42,43 @@ class ViewController: UIViewController {
         //
         
     }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        let VC = SecondViewController()
-//        VC.view.backgroundColor = . systemYellow
-//        VC.title = "서브 타이틀"
-        self.navigationController?.pushViewController(VC, animated: true)
+    
+    func setupButton() {
+        let button = UIButton(type: .system)
+        button.setTitle("다음 화면으로", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 30)
+        button.addAction(UIAction { [weak self] _ in
+            // 다음 화면으로 이동 세그웨이 실행
+            self?.performSegue(withIdentifier: "showDetailSegue", sender: nil)
+        }, for: .touchUpInside)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetailSegue",
+           let vc = segue.destination as? SecondViewController{
+            vc.title = "다음 화면"
+            vc.view.backgroundColor = .systemRed
+        }
+    }
+    //    override func viewDidAppear(_ animated: Bool) {
+    //        super.viewDidAppear(animated)
+    //        let VC = SecondViewController()
+    //        //        VC.view.backgroundColor = . systemYellow
+    //        //        VC.title = "서브 타이틀"
+    //        self.navigationController?.pushViewController(VC, animated: true)
+    //    }
 }
 
 #Preview {
-    UINavigationController(rootViewController: ViewController())
+    //    UINavigationController(rootViewController: ViewController())
+    
+    UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()!
 }
