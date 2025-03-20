66d7c4128fba1fd7ba521ca2cf0afe40ca0d2ee1
837100 <no8371@gmail.com>
2025-03-20 10:43:27 +0900

    섹션 나눠서 표시되도록 설정

---
+++ 07-UIKit/TableViewDemo/TableViewDemo/CustomCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomCell.swift
+//  ToDoUIKit
+//
+//  Created by NO SEONGGYEONG on 3/20/25.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
