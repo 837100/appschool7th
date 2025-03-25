cd131b3d440f8e945e3b762bcc7ab62792bb1fbf
837100 <no8371@gmail.com>
2025-03-25 14:23:52 +0900

    뷰 컨트롤러 삭제 및 저널 리스트 뷰 컨트롤러 추가: 테이블 뷰 및 언와인드 액션 구현

---
+++ 08-JRNL/JRNL/JRNL/JournalListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  JRNL
+//
+//  Created by NO SEONGGYEONG on 3/25/25.
+//
+
+import UIKit
+
+class JournalListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func unwindNewEntryCancel(segue: UIStoryboardSegue) {
+        print("unwindNewEntryCancel")
+    }
+}
+
+extension JournalListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
+    }
+}
