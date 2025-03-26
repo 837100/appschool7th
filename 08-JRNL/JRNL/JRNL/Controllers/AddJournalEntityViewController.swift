74e6a8c0f8f1f42adf7fa0f05b47c50fd078dc6e
837100 <no8371@gmail.com>
2025-03-26 14:37:18 +0900

    저널 리스트 뷰 컨트롤러 및 셀 추가, 저널 항목 모델 구현 저장 안됨 수정 필요

---
+++ 08-JRNL/JRNL/JRNL/Controllers/AddJournalEntityViewController.swift
@@ -0,0 +1,86 @@
+//
+//  AddJournalEntityViewController.swift
+//  JRNL
+//
+//  Created by NO SEONGGYEONG on 3/26/25.
+//
+
+import UIKit
+
+class AddJournalEntryViewController: UIViewController {
+
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+  
+    var newJournalEntry: JournalEntry?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 스토리보드 내에서 delegate를 설정해줬기 때문에 아래 코드는 필요 없음.
+        titleTextField.delegate = self
+        bodyTextView.delegate = self
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("prepare: \(String(describing: segue.identifier))")
+        if let segueIndentifier = segue.identifier {
+            if segueIndentifier == "save" {
+                let title = titleTextField.text ?? ""
+                let body = bodyTextView.text ?? ""
+                let photo = photoImageView.image
+                let rating = 3
+                newJournalEntry = JournalEntry(rating: rating, title: title, body: body, photo: photo)
+            }
+        }
+    }
+    
+    // MARK: - Methods
+    func updateSaveButtonState() {
+        let titleText = titleTextField.text ?? ""
+        let bodyText = bodyTextView.text ?? ""
+        saveButton.isEnabled = !titleText.isEmpty && !bodyText.isEmpty
+    }
+}
+
+extension AddJournalEntryViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("1 textFieldDidBeginEditing")
+        saveButton.isEnabled = false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("textFieldShouldReturn")
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateSaveButtonState()
+    }
+}
+
+extension AddJournalEntryViewController: UITextViewDelegate {
+    // 텍스트 뷰가 편집을 시작할 때 호출
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        print("2 textViewDidBeginEditing")
+        saveButton.isEnabled = false
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        print("shouldChangeTextIn: \(text)")
+        if text == "\n" {
+            textView.resignFirstResponder()
+        }
+        return true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        updateSaveButtonState()
+    }
+}
---
+++ 08-JRNL/JRNL/JRNL/Views/JournalListTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  JournalListTableViewCell.swift
+//  JRNL
+//
+//  Created by NO SEONGGYEONG on 3/26/25.
+//
+
+import UIKit
+
+class JournalListTableViewCell: UITableViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+}
