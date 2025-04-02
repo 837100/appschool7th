c84580eda314af44f897f3d2e0341d705b35319b
837100 <no8371@gmail.com>
2025-04-02 11:31:56 +0900

    위젯에 데이터가 표시되도록 수정

--- 08-JRNL/JRNLWidget/JRNLWidget.swift
+++ 08-JRNL/JRNLWidget/JRNLWidget.swift
@@ -12,12 +12,12 @@ struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), journalEntryDate: "JRNL", journalEntryTitle: "")
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        let entry = SimpleEntry(date: Date(), journalEntryDate: "JRNL", journalEntryTitle: "")
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
         
@@ -26,26 +26,22 @@ struct Provider: TimelineProvider {
         SharedData.shared.fetchJournalEntries()
         let journalEntries = SharedData.shared.getAllJournalEntries()
         
-        for minuteOffSet in 0..<journalEntries.count {
-            let entryDate = currentDate.addingTimeInterval(TimeInterval(60 * minuteOffSet))
-            let entry = SimpleEntry(date: , journalEntryDate: <#T##String#>, journalEntryTitle: <#T##String#>)
-        }
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+        for minutesOffset in 0..<journalEntries.count {
+            let entryDate = currentDate.addingTimeInterval(TimeInterval(60 * minutesOffset))
+            let entry = SimpleEntry(
+                date: entryDate,
+                journalEntryDate: journalEntries[minutesOffset].title ?? "JRNL",
+                journalEntryTitle: journalEntries[minutesOffset].entryTitle)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
-
-//    func relevances() async -> WidgetRelevances<Void> {
-//        // Generate a list containing the contexts this widget is relevant in.
-//    }
+    
+    //    func relevances() async -> WidgetRelevances<Void> {
+    //        // Generate a list containing the contexts this widget is relevant in.
+    //    }
 }
 
 struct SimpleEntry: TimelineEntry {
@@ -56,21 +52,18 @@ struct SimpleEntry: TimelineEntry {
 
 struct JRNLWidgetEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
         VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Emoji:")
-            Text(entry.emoji)
+            Text(entry.journalEntryDate)
+            Text(entry.journalEntryTitle)
         }
     }
 }
 
 struct JRNLWidget: Widget {
     let kind: String = "JRNLWidget"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             if #available(iOS 17.0, *) {
@@ -84,12 +77,14 @@ struct JRNLWidget: Widget {
         }
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
+        .supportedFamilies([.systemMedium, .accessoryRectangular])
     }
 }
 
 #Preview(as: .systemSmall) {
     JRNLWidget()
 } timeline: {
-    SimpleEntry(date: .now, emoji: "😀")
-    SimpleEntry(date: .now, emoji: "🤩")
+    SimpleEntry(date: .now, journalEntryDate: "2025년 4월 22일", journalEntryTitle: "오늘은 좋은 날입니다.")
+    SimpleEntry(date: .now, journalEntryDate: "2025년 4월 22일", journalEntryTitle: "오늘은 나쁜 날입니다.")
+    SimpleEntry(date: .now, journalEntryDate: "2025년 4월 22일", journalEntryTitle: "오늘은 그저 그런 날입니다.")
 }
