28be4c55378eb95dc9648deed5ba936d2464a2dd
837100 <no8371@gmail.com>
2025-04-02 16:46:08 +0900

    xrOS 지원을 위한 GlobeView 추가 및 MapViewController 수정

---
+++ 08-JRNL/JRNL/GlobeView.swift
@@ -0,0 +1,24 @@
+//
+//  GlobeView.swift
+//  JRNL
+//
+//  Created by NO SEONGGYEONG on 4/2/25.
+//
+
+import SwiftUI
+import RealityKit
+
+struct GlobeView: View {
+    var body: some View {
+        #if os(xrOS)
+        VStack {
+            Model3D(named: "globe") { model in
+                model.resizable()
+                    .aspectRatio(contentMode: .fit)
+            } placeholder: {
+                ProgressView()
+            }
+        }
+        #endif
+    }
+}
