13597d2a60a3d1aabe7b53e268e5b69e2ff5b82a
837100 <no8371@gmail.com>
2025-04-09 13:33:21 +0900

    OnDemandBookDetailsViewWithCombine: 뷰 모델 초기화 시 책 정보를 가져오는 로직 통합 및 불필요한 fetchBook 메서드 제거

--- 09-Combine/CombineFirebaseBookShelf/CombineFirebaseBookShelf/Combines/OnDemandBookDetailsViewWithCombine.swift
+++ 09-Combine/CombineFirebaseBookShelf/CombineFirebaseBookShelf/Combines/OnDemandBookDetailsViewWithCombine.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import FirebaseFirestore
 import FirebaseFirestoreCombineSwift
+import Combine
 
 private class BookDetailViewModel: ObservableObject {
     @Published var book = Book.empty
@@ -15,10 +16,14 @@ private class BookDetailViewModel: ObservableObject {
     
     private var db = Firestore.firestore()
     
-    fileprivate func fetchBook() {
+    init() {
         db.collection("books").document("hitchhiker").getDocument()
             .tryMap { documentSnapshot in
                 try documentSnapshot.data(as: Book.self)
+            }
+            .catch { error in
+                self.errorMessage = error.localizedDescription
+                return Just(Book.empty).eraseToAnyPublisher()
             }
             .replaceError(with: Book.empty)
             .assign(to: &$book)
@@ -48,12 +53,6 @@ struct OnDemandBookDetailsViewWithCombine: View {
             }
         }
         .navigationTitle("Book Details")
-        .onAppear() {
-            viewModel.fetchBook()
-        }
-        .refreshable {
-            viewModel.fetchBook()
-        }
     }
 }
 
