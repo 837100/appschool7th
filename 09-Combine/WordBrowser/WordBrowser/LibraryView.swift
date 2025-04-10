b154d6005830af5e94bf8d5dd9699df1ee4c452c
837100 <no8371@gmail.com>
2025-04-10 13:12:26 +0900

    WordBrowser: 초기 프로젝트 파일 및 뷰 추가

---
+++ 09-Combine/WordBrowser/WordBrowser/LibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryView.swift
+//  WordBrowser
+//
+//  Created by NO SEONGGYEONG on 4/10/25.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    LibraryView()
+}
---
+++ 09-Combine/WordBrowser/WordBrowser/WordSearchView.swift
@@ -0,0 +1,21 @@
+//
+//  WordSearchView.swift
+//  WordBrowser
+//
+//  Created by NO SEONGGYEONG on 4/10/25.
+//
+
+import SwiftUI
+
+struct WordSearchView: View {
+    var body: some View {
+        VStack {
+            EmptyView()
+                .navigationTitle("라이브러리")
+        }
+    }
+}
+
+#Preview {
+    WordSearchView()
+}
