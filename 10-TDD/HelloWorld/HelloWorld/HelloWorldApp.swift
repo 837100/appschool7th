a8c0e645d80220c2c457842d34d964c896cc0ec9
837100 <no8371@gmail.com>
2025-04-14 14:25:59 +0900

    HelloWorld 프로젝트 초기 설정 및 FizzBuzz 함수 구현

---
+++ 10-TDD/HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by NO SEONGGYEONG on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
