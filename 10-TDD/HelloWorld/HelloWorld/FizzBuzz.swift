22f94b3a9bd2fb3e42739423cf2731f1674619aa
837100 <no8371@gmail.com>
2025-04-14 15:35:26 +0900

    FizzBuzz 함수 구현 및 asyncSum 함수 추가

--- 10-TDD/HelloWorld/HelloWorld/FizzBuzz.swift
+++ 10-TDD/HelloWorld/HelloWorld/FizzBuzz.swift
@@ -6,5 +6,18 @@
 //
 
 func fizzBuzz(_ number: Int) -> String {
-    return ""
+    if number % 3 == 0 && number % 5 == 0 {
+        return "fizz-buzz"
+    } else if number % 3 == 0 {
+        return "fizz"
+    } else if number % 5 == 0 {
+        return "buzz"
+    } else {
+        return "\(number)"
+    }
+}
+
+func asyncSum(a: Int, b: Int, complete: @escaping (Int) -> Void) async {
+    try? await Task.sleep(nanoseconds: 200_000_000)
+    complete( a + b )
 }
