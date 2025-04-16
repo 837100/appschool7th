f5bb22434e5549ffd891926d0eed85cff6154fd5
837100 <no8371@gmail.com>
2025-04-16 14:21:24 +0900

    MenuList 및 MenuRow 수정: 메뉴 항목을 Result 타입으로 처리하여 오류 메시지 표시 기능 추가, MenuRow 뷰에서 가격 표시 추가, 관련 테스트 수정

--- 10-TDD/Albertos/AlbertosTests/MenuRowViewModelTests.swift
+++ 10-TDD/Albertos/AlbertosTests/MenuRowViewModelTests.swift
@@ -29,17 +29,18 @@ final class MenuRowViewModelTests: XCTestCase {
     }
     
     func testCallsGivenGroupingFunction() throws {
-            // 테스트 코드를 사용하지 않는 경우, Skip을 활용할 수 있다.
+        // 테스트 코드를 사용하지 않는 경우, Skip을 활용할 수 있다.
         try XCTSkipIf(true, "skipping this for now, keeping it to reuse part of the code later on")
-
+        
         var called = false
-        let inputSections = [MenuSection.fixture()]
+        let inputSections : [MenuSection] = [.fixture()]
         let spyClosure: ([MenuItem]) -> [MenuSection] = { items in
             called = true
             return inputSections
         }
-        let viewModel = MenuList.ViewModel(menuFetching: MenuFetchingPlaceholder(), menuGrouping: spyClosure)
-        let sections = viewModel.sections
+        let menuItems: [MenuItem] = inputSections.flatMap { $0.items }
+        let viewModel = MenuList.ViewModel(menuFetching: MenuFetchingStub( returning: .success(menuItems)), menuGrouping: spyClosure)
+        let sections = try viewModel.sections.get()
         // 주어진 클로저가 호출되었는지 확인합니다.
         XCTAssertTrue(called)
         // 반환된 값이 클로저로 생성된 값인지 확인합니다.
