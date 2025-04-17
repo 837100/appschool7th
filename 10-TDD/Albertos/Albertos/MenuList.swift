68a764551db26319040fccbb5bb3ec7c738d829c
837100 <no8371@gmail.com>
2025-04-17 09:33:14 +0900

    MenuFetchingPlaceholder 수정: 랜덤하게 실패와 성공이 되도록 수정 MenuList 수정: 메뉴 조회 실패 시 재시도 버튼 추가 및 관련 로직 개선

--- 10-TDD/Albertos/Albertos/MenuList.swift
+++ 10-TDD/Albertos/Albertos/MenuList.swift
@@ -25,7 +25,17 @@ struct MenuList: View {
             case .failure(let error):
                 Text("An error occurred:")
                 Text(error.localizedDescription).italic()
+                Button("Retry") {
+                    viewModel.retry()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
             }
+        }
+        .refreshable {
+            viewModel.retry()
         }
         .navigationTitle("Alberto's 🇮🇹")
     }
@@ -35,28 +45,42 @@ extension MenuList {
     class ViewModel: ObservableObject {
         @Published private(set) var sections: Result<[MenuSection], Error> = .success([])
         
+        private let menuFetching: MenuFetching
+        private let menuGrouping: ([MenuItem]) -> [MenuSection]
+        
         var cancellables = Set<AnyCancellable>()
         
         init(
             menuFetching: MenuFetching,
             menuGrouping: @escaping ([MenuItem]) -> [MenuSection] = groupMenuByCategory) {
-                
-                menuFetching.fetchMenu()
-                // [중요] 전달 받은 [MenuItem] 시퀀스를 menuGrouping 함수에 전달하도록 수정
-                    .map(menuGrouping)
-                // [중요] Result<[MenuSection], Error> 타입으로 변환
-                    .sink(receiveCompletion: { [weak self] completion in
-                        if case .failure(let error) = completion {
-                            self?.sections = .failure(error)
-                        }
-                    },
-                          receiveValue: { [weak self] value in
-                        self?.sections = .success(value)
-                    })
-                    .store(in: &cancellables)
+                self.menuFetching = menuFetching
+                self.menuGrouping = menuGrouping
+                fetchMenu()
             }
+        
+        func fetchMenu() {
+            menuFetching
+                .fetchMenu()
+            // [중요] 전달 받은 [MenuItem] 시퀀스를 menuGrouping 함수에 전달하도록 수정
+                .map(menuGrouping)
+            // [중요] Result<[MenuSection], Error> 타입으로 변환
+                .sink(receiveCompletion: { [weak self] completion in
+                    if case .failure(let error) = completion {
+                        self?.sections = .failure(error)
+                    }
+                },
+                      receiveValue: { [weak self] value in
+                    self?.sections = .success(value)
+                })
+                .store(in: &cancellables)
+        }
+        
+        func retry() {
+            fetchMenu()
+        }
     }
 }
+
 
 #Preview {
     NavigationStack {
