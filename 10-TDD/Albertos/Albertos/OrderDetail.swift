e4509208592b6b907986dbcbe65eeb24080be995
837100 <no8371@gmail.com>
2025-04-18 10:07:06 +0900

    주문 상세 화면 추가 및 OrderButton에서 주문 상세로의 네비게이션 기능 구현

---
+++ 10-TDD/Albertos/Albertos/OrderDetail.swift
@@ -0,0 +1,36 @@
+//
+//  OrderDetail.swift
+//  Albertos
+//
+//  Created by NO SEONGGYEONG on 4/18/25.
+//
+
+import SwiftUI
+
+struct OrderDetail: View {
+    @StateObject private var viewModel: ViewModel
+    
+    init(orderController: OrderController) {
+        _viewModel = StateObject(
+            wrappedValue: ViewModel(orderController: orderController)
+        )
+    }
+    
+    var body: some View {
+        List {
+            // TODO: 주문 목록을 보여주는 UI
+        }
+        .navigationTitle("주문 상세")
+    }
+}
+
+extension OrderDetail {
+    class ViewModel: ObservableObject {
+        let orderController: OrderController
+        
+        init(orderController: OrderController) {
+            self.orderController = orderController
+        }
+    }
+}
+
