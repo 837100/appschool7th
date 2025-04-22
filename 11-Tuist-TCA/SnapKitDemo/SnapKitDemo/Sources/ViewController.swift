bd1ee216c1acb1b109ee6b11417d0d4bbb8621ba
837100 <no8371@gmail.com>
2025-04-22 10:09:56 +0900

    RGB 뷰 배치로 실습

--- 11-Tuist-TCA/SnapKitDemo/SnapKitDemo/Sources/ViewController.swift
+++ 11-Tuist-TCA/SnapKitDemo/SnapKitDemo/Sources/ViewController.swift
@@ -6,25 +6,53 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private let redView = UIView()
+    private let blueView = UIView()
+    private let greenView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "SnapKit Demo"
         view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupConstraints()
     }
     
+    func setupViews() {
+        redView.backgroundColor = .red
+        blueView.backgroundColor = .blue
+        greenView.backgroundColor = .green
+        
+        view.addSubview(redView)
+        view.addSubview(blueView)
+        view.addSubview(greenView)
+    }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    func setupConstraints() {
+        redView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.leading.equalToSuperview().offset(20)
+            make.width.height.equalTo(100)
+        }
+        
+        blueView.snp.makeConstraints { make in
+            make.top.equalTo(redView)
+            make.trailing.equalToSuperview().offset(-20)
+            make.width.height.equalTo(redView)
+        }
+        
+        greenView.snp.makeConstraints { make in
+            make.top.equalTo(redView.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+            make.width.height.equalTo(150)
+        }
+    }
+}
+
+#Preview {
+    UINavigationController(rootViewController: ViewController())
 }
