1c982d18a2a5c730103fcc7dedf4f15c09d4ce97
837100 <no8371@gmail.com>
2025-04-22 09:52:29 +0900

    프로젝트 구조 개선: AppDelegate 추가 및 ContentView, SnapKitDemoApp 제거; Bundle ID 접두사 통일

---
+++ 11-Tuist-TCA/SnapKitDemo/SnapKitDemo/Sources/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SnapKitDemo
+//
+//  Created by NO SEONGGYEONG on 4/22/25.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
+
