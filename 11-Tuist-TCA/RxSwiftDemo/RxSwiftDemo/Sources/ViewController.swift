623c856ed8fcd5f5ab06b4f7bb204024fc34f4c9
837100 <no8371@gmail.com>
2025-04-22 14:35:07 +0900

    RxSwiftDemo 프로젝트 초기 설정 및 기본 파일 추가

---
+++ 11-Tuist-TCA/RxSwiftDemo/RxSwiftDemo/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftDemo
+//
+//  Created by NO SEONGGYEONG on 4/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "RxSwift Demo"
+    }
+    
+    
+    
+}
